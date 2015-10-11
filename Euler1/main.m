ea35061cb02a1e855f44b83bf8b01825eb1ef11f
JohanLundgren <johanlundgren@Catrins-Air.lan>
2015-10-11 20:16:28 +0200

    Initial Commit

---
+++ Euler1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Euler1
+//
+//  Created by JohanLundgren on 2015-10-11.
+//  Copyright (c) 2015 SuperMilkApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
